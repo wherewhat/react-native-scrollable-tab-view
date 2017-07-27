4ffc3c7e89fa98932d7842c1b53ae81e0aae28c9
Hector Aristy <hectoraristy@Hectors-MacBook-Pro.local>
2017-07-27 18:23:46 -0400

    added dynamic customTabBar.

--- DefaultTabBar.js
+++ DefaultTabBar.js
@@ -1,38 +1,57 @@
-const React = require('react');
-const { ViewPropTypes } = ReactNative = require('react-native');
-const {
+import React,{ Component } from 'react';
+import { 
+  ViewPropTypes,
   StyleSheet,
   Text,
   View,
   Animated,
-} = ReactNative;
+} from 'react-native';
+
 const Button = require('./Button');
 
-const DefaultTabBar = React.createClass({
-  propTypes: {
-    goToPage: React.PropTypes.func,
-    activeTab: React.PropTypes.number,
-    tabs: React.PropTypes.array,
-    backgroundColor: React.PropTypes.string,
-    activeTextColor: React.PropTypes.string,
-    inactiveTextColor: React.PropTypes.string,
-    textStyle: Text.propTypes.style,
-    tabStyle: ViewPropTypes.style,
-    renderTab: React.PropTypes.func,
-    underlineStyle: ViewPropTypes.style,
-  },
+class DefaultTabBar extends Component { 
+  constructor(props){
+    super(props);
+    this.renderTab = this.renderTab.bind(this);
+    this.renderTabOption = this.renderTabOption.bind(this);
+    this.generateSyleOption = this.generateSyleOption.bind(this);
+    this.styles = StyleSheet.create({
+      tab: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingBottom: 10,
+      },
+      flexOne: {
+        flex: 1,
+      },
+      tabs: {
+        height: 50,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        borderWidth: 1,
+        borderTopWidth: 0,
+        borderLeftWidth: 0,
+        borderRightWidth: 0,
+        borderColor: '#ccc',
+      },
+   });
+  }
 
-  getDefaultProps() {
-    return {
-      activeTextColor: 'navy',
-      inactiveTextColor: 'black',
-      backgroundColor: null,
-      height: 50
-    };
-  },
+  generateSyleOption(styles) {
+    let style = StyleSheet.flatten(this.styles);
+    Object.keys(style).forEach((element) => {
+      style[element] = StyleSheet.flatten(style[element])
+    });
+    let newStyle = StyleSheet.create({
+      ...style,
+      ...styles
+    });
+    this.styles = newStyle;
+  }
 
   renderTabOption(name, page) {
-  },
+  }
 
   renderTab(name, page, isTabActive, onPressHandler) {
     const { activeTextColor, inactiveTextColor, textStyle, } = this.props;
@@ -40,20 +59,20 @@ const DefaultTabBar = React.createClass({
     const fontWeight = isTabActive ? 'bold' : 'normal';
 
     return <Button
-      style={styles.flexOne}
+      style={this.styles.flexOne}
       key={name}
       accessible={true}
       accessibilityLabel={name}
       accessibilityTraits='button'
       onPress={() => onPressHandler(page)}
     >
-      <View style={[styles.tab, this.props.tabStyle, ]}>
+      <View style={[this.styles.tab, this.props.tabStyle, ]}>
         <Text style={[{color: textColor, fontWeight, }, textStyle, ]}>
           {name}
         </Text>
       </View>
     </Button>;
-  },
+  }
 
   render() {
     const containerWidth = this.props.containerWidth;
@@ -70,7 +89,7 @@ const DefaultTabBar = React.createClass({
       inputRange: [0, 1, ], outputRange: [0,  containerWidth / numberOfTabs, ],
     });
     return (
-      <View style={[styles.tabs, {backgroundColor: this.props.backgroundColor, }, this.props.style,{height: this.props.height},]}>
+      <View style={[this.styles.tabs, {backgroundColor: this.props.backgroundColor, }, this.props.style,{height: this.props.height},]}>
         {this.props.tabs.map((name, page) => {
           const isTabActive = this.props.activeTab === page;
           const renderTab = this.props.renderTab || this.renderTab;
@@ -79,29 +98,29 @@ const DefaultTabBar = React.createClass({
         <Animated.View style={[tabUnderlineStyle, { left, }, this.props.underlineStyle, ]} />
       </View>
     );
-  },
-});
+  }
+}
 
-const styles = StyleSheet.create({
-  tab: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingBottom: 10,
-  },
-  flexOne: {
-    flex: 1,
-  },
-  tabs: {
-    height: 50,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    borderWidth: 1,
-    borderTopWidth: 0,
-    borderLeftWidth: 0,
-    borderRightWidth: 0,
-    borderColor: '#ccc',
-  },
-});
+
+
+DefaultTabBar.propTypes = {
+  goToPage: React.PropTypes.func,
+  activeTab: React.PropTypes.number,
+  tabs: React.PropTypes.array,
+  backgroundColor: React.PropTypes.string,
+  activeTextColor: React.PropTypes.string,
+  inactiveTextColor: React.PropTypes.string,
+  textStyle: Text.propTypes.style,
+  tabStyle: ViewPropTypes.style,
+  renderTab: React.PropTypes.func,
+  underlineStyle: ViewPropTypes.style,
+}
+
+DefaultTabBar.defaultProps = {
+  activeTextColor: 'navy',
+  inactiveTextColor: 'black',
+  backgroundColor: null,
+  height: 50
+}
 
 module.exports = DefaultTabBar;
